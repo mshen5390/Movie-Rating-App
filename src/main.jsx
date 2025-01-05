55debbf5d49b88590f1c5a22a47ca25717c71ec2
mshen5390 <140674495+mshen5390@users.noreply.github.com>
2025-01-05 12:32:56 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+// npm run dev (to run the vite server on a local host)
